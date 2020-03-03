f6f8f383f600ba22685fe9707ea890af12361fb6
Daniel Otieno <danielotieno.ke@gmail.com>
2020-03-03 13:49:47 +0300

    chore:create user model

---
+++ task-manager-v2/database/models/user.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    tasks: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Task',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model('User', userSchema);
