d573b106e5b3a1993d61b9d605903a1a30655b03
Daniel Otieno <danielotieno.ke@gmail.com>
2020-03-03 13:52:21 +0300

    chore:create task model schema

---
+++ task-manager-v2/database/models/task.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Task',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model('Task', taskSchema);
